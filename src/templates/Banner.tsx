c2a69e600a1561a2c0e660e35795cafd9f2a69cc
beingutsav <utsavgarg@outlook.com>
2024-04-14 13:20:14 +0530

    Updated Banner to fetch contact number from central app config file.

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -1,12 +1,13 @@
 import { Button } from '../button/Button';
 import { CTABanner } from '../cta/CTABanner';
 import { Section } from '../layout/Section';
+import { AppConfig } from '../utils/AppConfig'; // Make sure to import AppConfig
 
 const Banner = () => (
   <Section>
     <CTABanner
       title="Gift your little one a great future!"
-      subtitle="Enroll Today! Contact : 9990961898"
+      subtitle={`Enroll Today! Contact : ${AppConfig.enquiry_number}`} // Use template literals
       button={<Button formId="contact-form-section">Start Learning</Button>}
     />
   </Section>
